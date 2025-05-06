6134df8ab57560a67a48148a3169a3449b836609
rudyravelindev <rudyravelindev@gmail.com>
2025-05-06 06:06:21 -0700

    Part 4: Header done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,15 @@
+import 'normalize.css';
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
-  return <div className="app">Hello World!</div>;
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+      </div>
+    </div>
+  );
 }
 
 export default App;
