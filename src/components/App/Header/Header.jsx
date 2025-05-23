2494a35209cc05d772781635a016c6f3613a7c3a
rudyravelindev <rudyravelindev@gmail.com>
2025-05-22 21:17:03 -0700

    feat: Add temperature unit toggle switch
    - Implement toggle switch component for F/C temperature conversion
    - Add toggle switch styling with proper unit display
    - Include interactive functionality to switch between Fahrenheit and Celsius

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,25 +1,70 @@
+// import './Header.css';
+// import logo from '../../../assets/logo.svg';
+// import avatar from '../../../assets/avatar.png';
+// import ToggleSwitch from '../../ToggleSwitch/ToggleSwitch';
+
+// function Header({ handleAddClick, weatherData }) {
+//   const currentDate = new Date().toLocaleString('default', {
+//     month: 'long',
+//     day: 'numeric',
+//   });
+//   return (
+//     <header className="header">
+//       <img src={logo} alt="Logo" className="header__logo" />
+
+//       <p className="header__date-and-location">
+//         {currentDate}, {weatherData.city}
+//       </p>
+//       <ToggleSwitch />
+//       <button
+//         onClick={handleAddClick}
+//         type="button"
+//         className="header__add-clothes-btn"
+//       >
+//         + Add Clothes
+//       </button>
+
+//       <div className="header__user-container">
+//         <p className="header__username">Terrence Tegegne</p>
+//         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Header;
+
 import './Header.css';
 import logo from '../../../assets/logo.svg';
 import avatar from '../../../assets/avatar.png';
+import ToggleSwitch from '../../ToggleSwitch/ToggleSwitch';
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString('default', {
     month: 'long',
     day: 'numeric',
   });
+
   return (
     <header className="header">
-      <img src={logo} alt="Logo" className="header__logo" />
-      <p className="header__date-and-location">
-        {currentDate}, {weatherData.city}
-      </p>
-      <button
-        onClick={handleAddClick}
-        type="button"
-        className="header__add-clothes-btn"
-      >
-        + Add Clothes
-      </button>
+      <div className="header__left-group">
+        <img src={logo} alt="Logo" className="header__logo" />
+        <p className="header__date-and-location">
+          {currentDate}, {weatherData.city}
+        </p>
+      </div>
+
+      <div className="header__toggle-add-group">
+        <ToggleSwitch />
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__add-clothes-btn"
+        >
+          + Add Clothes
+        </button>
+      </div>
+
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
