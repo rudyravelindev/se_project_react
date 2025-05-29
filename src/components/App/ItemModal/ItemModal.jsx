153cf63029071f984643d53d0a36f073a36a6876
rudyravelindev <rudyravelindev@gmail.com>
2025-05-28 19:45:40 -0700

    clean up

--- src/components/App/ItemModal/ItemModal.jsx
+++ src/components/App/ItemModal/ItemModal.jsx
@@ -1,38 +1,3 @@
-// import '../ItemModal/ItemModal.css';
-// import CloseIconBtn from '../../../assets/close-btn.svg';
-
-// function ItemModal({ activeModal, onClose, card, isOpen, onDelete }) {
-//   return (
-//     <div className={`modal ${isOpen && 'modal_opened'}`}>
-//       <div className="modal__content modal__content_type_image">
-//         <button onClick={onClose} className="modal__close">
-//           <img
-//             src={CloseIconBtn}
-//             alt="Close modal"
-//             width={20}
-//             height={20}
-//             className="modal__close-icon"
-//           />
-//         </button>
-//         <img
-//           src={card.link || card.imageUrl}
-//           alt={card.name || 'Preview image'}
-//           className="modal__image"
-//         />
-//         <div className="modal__footer">
-//           <h2 className="modal__caption">{card.name}</h2>
-//           <p className="modal__weather">Weather: {card.weather}</p>
-//         </div>
-//         <button className="modal__delete" onClick={() => onDelete(card)}>
-//           Delete item
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default ItemModal;
-
 import '../ItemModal/ItemModal.css';
 import CloseIconBtn from '../../../assets/close-btn.svg';
 
