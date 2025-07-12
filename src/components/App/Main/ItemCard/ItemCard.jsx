c0ffca8c602deca4fcef7d74e5b87a7244b67922
rudyravelindev <rudyravelindev@gmail.com>
2025-07-12 00:54:19 -0700

    updating the server

--- src/components/App/Main/ItemCard/ItemCard.jsx
+++ src/components/App/Main/ItemCard/ItemCard.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import CurrentUserContext from '../../../../contexts/CurrentUserContext';
+import CurrentUserContext from '../../../../contexts/CurrentUserContext.js';
 
 import './ItemCard.css';
 
